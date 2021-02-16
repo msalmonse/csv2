daeddf86a50774a1e01ae3fb324827353f1850a0
Michael Salmon <msalmonse@gmail.com>
2021-02-16 15:17:22 +0100

    Added CSV and Settings handling

--- csv2svg/main.swift
+++ csv2svg/main.swift
@@ -7,5 +7,9 @@
 
 import Foundation
 
-print("Hello, World!")
+let csvName = CommandLine.argc > 1 ? CommandLine.arguments[1] : "/Users/mesa/xcode/Fibonacci/timing.csv"
+let plistName = CommandLine.argc > 2 ? CommandLine.arguments[2] : csvName + ".plist"
 
+let settings = Settings(plistName)
+
+let csv = try CSV(csvName)
