58521f0314b50bd967a870bb5e626e78695776f7
Michael Salmon <msalmonse@gmail.com>
2021-02-18 12:39:49 +0100

    Added conditional json decoding

--- csv2svg/Settings.swift
+++ csv2svg/Settings.swift
@@ -7,12 +7,36 @@
 
 import Foundation
 
-class Settings {
+class Settings: Codable {
     // svg width and height
-    let height = 0
-    let width = 0
+    let height: Int
+    let width: Int
     
-    init(_ name: String) {
-        print(name)
+    // svg title
+    let title: String
+    
+    required init(from decoder: Decoder) throws {
+        do {
+            let container = try decoder.container(keyedBy: CodingKeys.self)
+            
+            height = (try? container.decodeIfPresent(Int.self, forKey: .height)) ?? 500
+            width = (try? container.decodeIfPresent(Int.self, forKey: .width)) ?? 500
+            title = (try? container.decodeIfPresent(String.self, forKey: .title)) ?? ""
+        } catch {
+            print(error)
+            throw(error)
+        }
+    }
+
+    static func load(_ name: String) throws -> Settings {
+        let url = URL(fileURLWithPath: name)
+        do {
+            let data = try Data(contentsOf: url)
+            let decoder = JSONDecoder()
+            let settings = try decoder.decode(Settings.self, from: data)
+            return settings
+        } catch {
+            throw(error)
+        }
     }
 }
