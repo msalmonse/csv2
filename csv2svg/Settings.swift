2c586dbef47653ffc99505296a4e88814c03baf7
Michael Salmon <msalmonse@gmail.com>
2021-02-22 15:30:57 +0100

    Added new settings

--- csv2svg/Settings.swift
+++ csv2svg/Settings.swift
@@ -15,8 +15,19 @@ class Settings: Codable {
     // svg title
     let title: String
     
+    // Header rows and columns
+    let headerColumns: Int
+    let headerRows: Int
+    
     // Index for x values in csv data
     let index: Int
+
+    // minimum and maximum for x and y axes
+    // nil means not specified
+    let xMax: Double?
+    let xMin: Double?
+    let yMax: Double?
+    let yMin: Double?
 
     // convenience function to decode a keyed Int
     private static func keyedIntValue(
@@ -41,10 +52,16 @@ class Settings: Codable {
     required init(from decoder: Decoder) {
         let container = try? decoder.container(keyedBy: CodingKeys.self)
 
-        height = Settings.keyedIntValue(from: container, forKey: .height, defaultValue: 500)
-        index = Settings.keyedIntValue(from: container, forKey: .index, defaultValue: 0)
-        title = Settings.keyedStringValue(from: container, forKey: .title, defaultValue: "")
-        width = Settings.keyedIntValue(from: container, forKey: .width, defaultValue: 500)
+        headerColumns = Self.keyedIntValue(from: container, forKey: .headerColumns, defaultValue: 0)
+        headerRows = Self.keyedIntValue(from: container, forKey: .headerRows, defaultValue: 0)
+        height = Self.keyedIntValue(from: container, forKey: .height, defaultValue: 500)
+        index = Self.keyedIntValue(from: container, forKey: .index, defaultValue: 0)
+        title = Self.keyedStringValue(from: container, forKey: .title, defaultValue: "")
+        width = Self.keyedIntValue(from: container, forKey: .width, defaultValue: 500)
+        xMax = try? container?.decodeIfPresent(Double.self, forKey: .xMax)
+        xMin = try? container?.decodeIfPresent(Double.self, forKey: .xMin)
+        yMax = try? container?.decodeIfPresent(Double.self, forKey: .yMax)
+        yMin = try? container?.decodeIfPresent(Double.self, forKey: .yMin)
     }
 
     static func load(_ name: String) throws -> Settings {
