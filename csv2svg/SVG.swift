fb314a2ba0a26d39ae5719ffaca82ddfb54c9040
Michael Salmon <msalmonse@gmail.com>
2021-02-24 13:14:38 +0100

    New parameters

--- csv2svg/SVG.swift
+++ csv2svg/SVG.swift
@@ -10,6 +10,15 @@ import Foundation
 class SVG {
     let csv: CSV
     let settings: Settings
+    
+    // Row or column to use for x values
+    let index: Int
+    
+    // The four sides of the plane
+    let top: Double
+    let bottom: Double
+    let left: Double
+    let right: Double
 
     // Tags
     let xmlTag = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>"
@@ -26,6 +35,10 @@ class SVG {
     init(_ csv: CSV, _ settings: Settings) {
         self.csv = csv
         self.settings = settings
+        
+        self.index = settings.index - 1
+        
+        (top, bottom, left, right) = SVG.sidesFromColumns(csv, settings)
     }
 
     func svgLineGroup() -> [String] {
