e86edcec222b4225063f632b4f8f044e15903594
Michael Salmon <msalmonse@gmail.com>
2021-02-24 12:57:54 +0100

    Added method to plot files

---
+++ csv2svg/SvgColumnPlot.swift
@@ -0,0 +1,20 @@
+//
+//  SvgColumnPlot.swift
+//  csv2svg
+//
+//  Created by Michael Salmon on 2021-02-23.
+//
+
+import Foundation
+
+extension SVG {
+   
+    /// Plot the non-index and non header columns
+    /// - Returns: An array of the path elements for the columns
+    
+    func columnPlot() -> [String] {
+        var paths: [String] = []
+
+        return paths
+    }
+}
