ab916bb8e57625ce38d30f4f399a09d3e1de686e
Michael Salmon <msalmonse@gmail.com>
2021-02-26 20:15:46 +0100

    Plot values from csv data

--- csv2svg/SvgColumnPlot.swift
+++ csv2svg/SvgColumnPlot.swift
@@ -8,13 +8,43 @@
 import Foundation
 
 extension SVG {
-   
+    
+    /// Calculate a list of x values, either a range or a data column
+    /// - Returns: list of x values
+    func xList() -> [Double?] {
+        if index < 0 {
+            return (settings.headerRows..<csv.data.count).map { Double($0) }
+        } else {
+            return csv.columnValues(index)
+        }
+        
+    }
+
     /// Plot the non-index and non header columns
     /// - Returns: An array of the path elements for the columns
     
     func columnPlot() -> [String] {
         var paths: [String] = []
 
+        let xValues = xList()
+        for i in 0..<csv.colCt {
+            if i != index {
+                var pathPoints: [PathCommand] = []
+                let yValues = csv.columnValues(i)
+                var move = true
+                for j in 0..<csv.rowCt {
+                    if xValues[j] == nil || yValues[j] == nil {
+                        move = true
+                    } else if move {
+                        pathPoints.append(.moveTo(x: xValues[j]!, y: yValues[j]!))
+                        move = false
+                    } else {
+                        pathPoints.append(.lineTo(x: xValues[j]!, y: yValues[j]!))
+                    }
+                }
+                paths.append(Self.svgPath(pathPoints))
+            }
+        }
         return paths
     }
 }
