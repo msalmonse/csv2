af1a29649a5cf126d57bc94082afb663b04d33c0
Michael Salmon <msalmonse@gmail.com>
2021-03-04 11:08:55 +0100

    changed version.swift to appinfo.swift

--- autoversion.sh
+++ autoversion.sh
@@ -4,7 +4,7 @@
 dest=${PROJECT_DIR:-$PWD}
 name=${1:-$PRODUCT_NAME}
 
-cat > "$dest/version.swift" <<EOD
+cat > "$dest/appinfo.swift" <<EOD
 //  Application version information automatically generated at build time.
 //  Do not commit this file to your code repository.
 
@@ -13,5 +13,6 @@ struct AppInfo {
 	static let version="$(git describe --always --dirty)"
 	static let build = $(git rev-list HEAD | wc -l | tr -d ' ')
 	static let hash = "$(git rev-parse HEAD)"
+	static let origin="$(git remote get-url origin)"
 }
 EOD
