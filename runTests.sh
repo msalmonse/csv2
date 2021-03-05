50c74e093e93ed8dbde60193390482ec8ce7ae3e
Michael Salmon <msalmonse@gmail.com>
2021-03-05 08:55:44 +0100

    Create svg files from test data

---
+++ runTests.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#  runTests.sh
+#  csv2svg
+#
+#  Created by Michael Salmon on 2021-03-05.
+#
+
+datadir=testdata
+svgdir=svg
+
+makeSVG () {
+    csv="$datadir/$1.csv"
+    json="$datadir/$2.json"
+    svg="$svgdir/$1-data+$2-json.svg"
+    ./test/csv2svg "$csv" "$json" > "$svg" \
+    && echo "$3" > "$svg.txt"
+}
+cat $datadir/testlist.txt \
+| while read csv json msg
+do
+    makeSVG "$csv" "$json" "$msg"
+done
