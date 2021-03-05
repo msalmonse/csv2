4634b34af96b79ee3ca42561ff3a8587252bddfc
Michael Salmon <msalmonse@gmail.com>
2021-03-05 09:48:19 +0100

    Added makeIndex to create index.html for svg files

---
+++ makeIndex.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#  makeIndex.sh
+#  csv2svg
+#
+#  Created by Michael Salmon on 2021-03-05.
+#  
+
+exec > svg/index.html
+
+# Headers
+cat <<EOD
+<html>
+<head>
+    <title>SVG test results</title>
+</head>
+<body>
+EOD
+
+for f in svg/*.svg
+do
+    cat <<EOD
+<h3>$(cat "$f.txt")</h3>
+<img src="$(basename "$f")" />
+EOD
+done
+
+# Footers
+cat <<EOD
+</body>
+</head>
+EOD
