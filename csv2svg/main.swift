3baf4fe8363e25dee69b2c2fdf8df6bc59b581ad
Michael Salmon <msalmonse@gmail.com>
2021-03-13 21:29:10 +0100

    Little refactoring

--- csv2svg/main.swift
+++ csv2svg/main.swift
@@ -7,42 +7,43 @@
 
 import Foundation
 
-var options = Options.parseOrExit()
-options.setDefaults()
+var opts = Options.parseOrExit()
+opts.setDefaults()
 
-if options.debug != 0 {
-    print(options, to: &standardError)
+if opts.debug != 0 {
+    print(opts, to: &standardError)
 }
 
-if options.version {
+if opts.version {
     print("\(AppInfo.name): \(AppInfo.version) (\(AppInfo.build)) Built at \(AppInfo.builtAt)",
           to: &standardError)
     exit(0)
 }
 
-if options.shapes {
+if opts.shapes {
     print(SVG.Shape.allNames())
     exit(0)
 }
 
-if options.show != "" {
-    print(showShape(shape: options.show, stroke: options.colour))
+if opts.show != "" {
+    print(showShape(shape: opts.show, stroke: opts.colour))
     exit(0)
 }
 
 // use a csvName of - to mean use stdin
-if options.csvName == "-" { options.csvName = nil }
+if opts.csvName == "-" { opts.csvName = nil }
 
-if options.verbose {
-    print(options.csvName ?? "Missing CSV file name", to: &standardError)
-    print(options.jsonName ?? "Missing JSON file name", to: &standardError)
+if opts.verbose {
+    print(opts.csvName ?? "Missing CSV file name", to: &standardError)
+    print(opts.jsonName ?? "Missing JSON file name", to: &standardError)
 }
 
-let settings = try? Settings.load(URL(fileURLWithPath: options.jsonName ?? ((options.csvName ?? "") + ".json")))
-if (options.debug & 2) > 0 { print(settings ?? "Nil settings", to: &standardError) }
+let jsonUrl = URL(fileURLWithPath: opts.jsonName ?? ((opts.csvName ?? "") + ".json"))
+let settings = try? Settings.load(jsonUrl)
+if (opts.debug & 2) > 0 { print(settings ?? "Nil settings", to: &standardError) }
 
-let csv = options.csvName != nil ? (try? CSV(URL(fileURLWithPath: options.csvName!))) : CSV(readLines())
-if (options.debug & 4) > 0 { print(csv ?? "Nil csv", to: &standardError) }
+let csv = opts.csvName != nil ? (try? CSV(URL(fileURLWithPath: opts.csvName!))) : CSV(readLines())
+if (opts.debug & 4) > 0 { print(csv ?? "Nil csv", to: &standardError) }
 
 if csv == nil || settings == nil {
     print("Error loading data.", to: &standardError)
@@ -50,6 +51,6 @@ if csv == nil || settings == nil {
 }
 
 let svg = SVG(csv!, settings!)
-if (options.debug & 8) > 0 { print(svg, to: &standardError) }
+if (opts.debug & 8) > 0 { print(svg, to: &standardError) }
 
 _ = svg.gen().map { print($0) }
