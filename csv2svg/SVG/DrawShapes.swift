9c9e51b5284ac237a60bd3d08968b9505c15987d
Michael Salmon <msalmonse@gmail.com>
2021-03-13 08:28:34 +0100

    Fixed some problems with line ends

--- csv2svg/SVG/DrawShapes.swift
+++ csv2svg/SVG/DrawShapes.swift
@@ -9,6 +9,10 @@ import Foundation
 
 extension SVG.PathCommand {
 
+    /// Generate a blade shape
+    /// - Parameter w: the width
+    /// - Returns: path string for a blade
+
     func drawBlade(w: Double) -> String {
         let half = w/2.0
         return [
@@ -22,6 +26,10 @@ extension SVG.PathCommand {
         ].map { $0.command()}.joined(separator: " ")
     }
 
+    /// Generate a circle
+    /// - Parameter r: the radius
+    /// - Returns: path string for a circle
+
     func drawCircle(r: Double) -> String {
         return [
             Self.moveBy(dx: 0, dy: -r),
@@ -31,16 +39,26 @@ extension SVG.PathCommand {
         ].map { $0.command() }.joined(separator: " ")
     }
 
+    /// Generate a diamond shape
+    /// - Parameter w: the width
+    /// - Returns: path string for a diamond
+
     func drawDiamond(w: Double) -> String {
+        let half = w/2.0
         return [
             Self.moveBy(dx: -w, dy: 0.0),
             .lineBy(dx: w, dy: -w),
             .lineBy(dx: w, dy: w),
             .lineBy(dx: -w, dy: w),
             .lineBy(dx: -w, dy: -w),
-            .moveBy(dx: w, dy: 0.0)
+            .lineBy(dx: half, dy: -half),
+            .moveBy(dx: half, dy: half)
         ].map { $0.command() }.joined(separator: " ")
     }
+
+    /// Generate a shuriken shape
+    /// - Parameter w: the width
+    /// - Returns: path string for a shuriken
 
     func drawShuriken(w: Double) -> String {
         let half = w/2.0
@@ -54,6 +72,10 @@ extension SVG.PathCommand {
         ].map { $0.command() }.joined(separator: " ")
     }
 
+    /// Generate a square
+    /// - Parameter w: the width
+    /// - Returns: path string for a square
+
     func drawSquare(w: Double) -> String {
         let w2 = w * 2.0
         return [
@@ -62,9 +84,14 @@ extension SVG.PathCommand {
             .vertBy(dy: w2),
             .horizBy(dx: -w2),
             .vertBy(dy: -w2),
-            .moveBy(dx: w, dy: w)
+            .horizBy(dx: w),
+            .moveBy(dx: 0.0, dy: w)
         ].map { $0.command() }.joined(separator: " ")
     }
+
+    /// Generate a star shape
+    /// - Parameter w: the width
+    /// - Returns: path string for a star
 
     func drawStar(w: Double) -> String {
         let half = w/2.0
@@ -78,14 +105,20 @@ extension SVG.PathCommand {
         ].map { $0.command() }.joined(separator: " ")
     }
 
+    /// Generate a triangle
+    /// - Parameter w: the width
+    /// - Returns: path string for a triangle
+
     func drawTriangle(w: Double) -> String {
         let w2 = w * 2.0
+        let half = w/2.0
         return [
             Self.moveBy(dx: 0.0, dy: -w),
             .lineBy(dx: w, dy: w2),
             .horizBy(dx: -w2),
             .lineBy(dx: w, dy: -w2),
-            .moveBy(dx: w, dy: 0.0)
+            .lineBy(dx: half, dy: w),
+            .moveBy(dx: -half, dy: 0.0)
         ].map { $0.command() }.joined(separator: " ")
     }
 }
