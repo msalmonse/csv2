51d8fe7a8e584fe17373c28afe015abaf08f1d29
Michael Salmon <msalmonse@gmail.com>
2021-03-16 20:28:39 +0100

    Added comment

--- csv2svg/SVG/PlotCommon.swift
+++ csv2svg/SVG/PlotCommon.swift
@@ -157,7 +157,7 @@ extension SVG {
                 state.plotOne(pos, clipped: clipped, plotShape: plotShape)
             }
         }
-        state.nilPlot(plotShape)
+        state.nilPlot(plotShape)        // handle any trailing singletons
         return Self.svgPath(state.pathPoints, stroke: props.colour, width: plotWidth)
     }
 }
