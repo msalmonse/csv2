ec6e4f131007fac15c9de7a27ccc88823abeb921
Michael Salmon <msalmonse@gmail.com>
2021-04-07 18:32:00 +0200

    Added JS class

---
+++ csv2/JS/JS.swift
@@ -0,0 +1,36 @@
+//
+//  JS.swift
+//  csv2
+//
+//  Created by Michael Salmon on 2021-04-07.
+//
+
+import Foundation
+
+class JS: Plotter {
+    func plotGroup(lines: String) -> String {
+        return ""
+    }
+
+    func plotHead(positions: Positions, plotPlane: Plane, propsList: PropertiesList) -> String {
+        return ""
+    }
+
+    func plotPath(_ points: [PathCommand], props: Properties) -> String {
+        return ""
+    }
+
+    func plotRect(_ plane: Plane, rx: Double, props: Properties) -> String {
+        return ""
+    }
+
+    func plotTail() -> String {
+        return ""
+    }
+
+    func plotText(x: Double, y: Double, text: String, props: Properties) -> String {
+        return ""
+    }
+
+
+}
