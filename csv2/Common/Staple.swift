cc7463ef7722efdfc43372d2f6a73671ae9fa697
Michael Salmon <msalmonse@gmail.com>
2021-04-11 20:15:26 +0200

    More staples

--- csv2/Common/Staple.swift
+++ csv2/Common/Staple.swift
@@ -14,18 +14,25 @@ struct Staple {
         current += 1
         return current
     }
+
+    let pixels: Double
 }
 
 extension Staple {
+
     /// Calculate the minimum difference between x values
     /// - Parameter xi: list of x values
     /// - Returns: minimum of x differences
-    
+
     static func minSpan(_ xi: [XIvalue]) -> Double {
         return xi.indices.dropFirst()
             .map { (xi[$0].x ?? Double.infinity) - (xi[$0 - 1].x ?? Double.infinity) }
             .reduce(Double.infinity) { min($0, $1) }
     }
+
+    /// Check that there are enough poxels for a staple diagram
+    /// - Parameter diff: pixels between x values
+    /// - Returns: true if there are
 
     static func spanOK(_ diff: Double) -> Bool {
         return Self.count > 0 && diff/Double(Self.count) > 5.0
--- csv2/Plot/StapleGet.swift
+++ csv2/Plot/StapleGet.swift
@@ -14,13 +14,13 @@ extension Plot {
     ///   - xi: list of x values
     ///   - ts: TransScale object
     /// - Returns: Scale object if OK or nil
-    
+
     func stapleGet(_ xi: [XIvalue], _ ts: TransScale) -> Staple? {
         if Staple.count <= 0 { return nil }
         let minδx = Staple.minSpan(xi)
         if minδx < 0.0 { return nil }
         let minδpixels = ts.xpos(minδx) - ts.xpos(minδx)
         if !Staple.spanOK(minδpixels) { return nil }
-        return Staple()
+        return Staple(pixels: minδpixels)
     }
 }
