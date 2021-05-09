4479e2e9675f35c205c4b72e0a41f8f8e9bf9690
Michael Salmon <msalmonse@gmail.com>
2021-05-09 08:07:32 +0200

    Don't scale if not needed

--- csv2/PNG/PNGPlotter.swift
+++ csv2/PNG/PNGPlotter.swift
@@ -9,15 +9,26 @@ import Foundation
 import AppKit
 
 extension PNG {
+    /// Start clipping to plotPlane
+    /// - Parameter plotPlane: the plane for plots
+
     func plotClipStart(plotPlane: Plane) {
         clipRect =
             CGRect(x: plotPlane.left, y: plotPlane.top, width: plotPlane.width, height: plotPlane.height)
     }
 
+    /// Stop clipping
+
     func plotClipEnd() {
         clipRect = nil
         ctx.resetClip()
     }
+
+    /// Initialize anything before we start plotting
+    /// - Parameters:
+    ///   - positions: where to place things
+    ///   - plotPlane: where the plots will go
+    ///   - stylesList: all styles
 
     func plotHead(positions: Positions, plotPlane: Plane, stylesList: StylesList) {
         if settings.plotter.logoURL.hasContent {
@@ -30,41 +41,62 @@ extension PNG {
         }
     }
 
+    /// Print the chart
+
     func plotPrint() {
         return
     }
 
-    func plotRect(_ plane: Plane, rx: Double, styles: Styles) {
-        return
-    }
+    /// End the chart
 
     func plotTail() {
         return
     }
 
-    func plotWrite(to url: URL) throws {
-        // NSImage uses points for size so we rescale the image to match the intended size
-        let cgImage = image.cgImage(forProposedRect: nil, context: nil, hints: nil)!
-        let scaleCtx = CGContext(
-            data: nil,
-            width: settings.dim.width,
-            height: settings.dim.height,
-            bitsPerComponent: cgImage.bitsPerComponent,
-            bytesPerRow: cgImage.bytesPerRow,
-            space: cgImage.colorSpace ?? CGColorSpace(name: CGColorSpace.sRGB)!,
-            bitmapInfo: cgImage.bitmapInfo.rawValue
-        )!
-        scaleCtx.interpolationQuality = .high
-        scaleCtx.draw(cgImage, in: CGRect(origin: .zero, size: image.size))
-        let scaled = scaleCtx.makeImage()!
+    /// Write a CGImage to a file after convering to png
+    /// - Parameters:
+    ///   - cgImage: the image to write
+    ///   - url: where to write
+    /// - Throws: file writing errors
 
-        let imageRep = NSBitmapImageRep(cgImage: scaled)
+    private func cgImageWrite(_ cgImage: CGImage, to url: URL) throws {
+        let imageRep = NSBitmapImageRep(cgImage: cgImage)
         if let pngData = imageRep.representation(using: .png, properties: [:]) {
             do {
                 try pngData.write(to: url)
             } catch {
                 throw(error)
             }
+        }
+    }
+
+    /// Write the image to a file after converting to png format
+    /// - Parameter url: where to write
+    /// - Throws: any error to do with file writing
+
+    func plotWrite(to url: URL) throws {
+        do {
+            let cgImage = image.cgImage(forProposedRect: nil, context: nil, hints: nil)!
+            if cgImage.width == settings.dim.width {
+                try cgImageWrite(cgImage, to: url)
+            } else {
+                // NSImage uses points for size so we rescale the image to match the intended size
+                let scaleCtx = CGContext(
+                    data: nil,
+                    width: settings.dim.width,
+                    height: settings.dim.height,
+                    bitsPerComponent: cgImage.bitsPerComponent,
+                    bytesPerRow: cgImage.bytesPerRow,
+                    space: cgImage.colorSpace ?? CGColorSpace(name: CGColorSpace.sRGB)!,
+                    bitmapInfo: cgImage.bitmapInfo.rawValue
+                )!
+                scaleCtx.interpolationQuality = .high
+                scaleCtx.draw(cgImage, in: CGRect(origin: .zero, size: image.size))
+                let scaled = scaleCtx.makeImage()!
+                try cgImageWrite(scaled, to: url)
+            }
+        } catch {
+            throw(error)
         }
     }
 }
