5f8a0c287471ebd073516eeea413c2112de9cbf5
Michael Salmon <msalmonse@gmail.com>
2021-05-25 18:39:11 +0200

    Fixed bug for rows

--- csv2/Plot/XIlist.swift
+++ csv2/Plot/XIlist.swift
@@ -18,7 +18,7 @@ extension Plot {
                 return (-settings.csv.headerColumns..<csv.colCt).map { XIvalue(x: Double($0), i: $0) }
             } else {
                 let val = csv.rowValues(index)
-                return (0 ..< csv.rowCt).map { XIvalue(x: val[$0], i: $0) }
+                return (0 ..< csv.colCt).map { XIvalue(x: val[$0], i: $0) }
             }
         } else {
             if index < 0 {
